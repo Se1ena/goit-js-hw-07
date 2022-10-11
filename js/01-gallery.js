6fc174e339a588b98508275aff2076979051f584
Veronika Moskaliuk <veronika.moskaliuk.ua@gmail.com>
2022-10-11 20:29:56 +0200

    done 1, 2 gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGallaryItemMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', itemsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGallaryItemMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </div>
+      `;
+    })
+    .join("");
+}
+
+let modalWindow;
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const isGalleryRef = evt.target.classList.contains('gallery__image');
+    if(!isGalleryRef){
+        return;
+    }
+    console.log(evt.target);
+
+    modalWindow = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}" width="800" heigth="600">
+`)
+
+modalWindow.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+let allImages = "";
+
+galleryItems.forEach((object) => {
+    allImages += `<a class="gallery__item" href="${object.original}"> 
+    <img class="gallery__image" src="${object.preview}" alt="${object.description}">
+    </a>`;
+});
+
+const ulRef = document.querySelector('ul.gallery');
+ulRef.innerHTML = allImages;
+
+var lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay:250});
